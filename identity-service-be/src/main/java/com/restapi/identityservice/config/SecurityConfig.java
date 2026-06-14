030d53064e35e1e0e966b99a6d50891af2af162b
lamnguyen296 <lamnbhe181873@fpt.edu.vn>
2026-06-14 22:53:24 +0700

    Fix CORS to allow Netlify origin

--- identity-service-be/src/main/java/com/restapi/identityservice/config/SecurityConfig.java
+++ identity-service-be/src/main/java/com/restapi/identityservice/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000")); // React vite default port
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
