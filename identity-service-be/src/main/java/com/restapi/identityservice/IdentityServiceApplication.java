47f4c51d3f61bc3e7f9d13e0ed3167fa89d7bdc5
lamnguyen296 <lamnbhe181873@fpt.edu.vn>
2026-06-14 23:30:51 +0700

    Fix timezone issue (set default to Asia/Ho_Chi_Minh)

--- identity-service-be/src/main/java/com/restapi/identityservice/IdentityServiceApplication.java
+++ identity-service-be/src/main/java/com/restapi/identityservice/IdentityServiceApplication.java
@@ -2,11 +2,17 @@ package com.restapi.identityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.context.annotation.Bean;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class IdentityServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IdentityServiceApplication.class, args);
