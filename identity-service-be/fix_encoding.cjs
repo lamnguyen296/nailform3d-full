9c3ab77c55234e5681521c13029c91537d485fc5
lamnguyen296 <lamnbhe181873@fpt.edu.vn>
2026-07-23 00:08:50 +0700

    Chore: Remove redundant files from tracking

--- identity-service-be/fix_encoding.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const file = 'd:/nailform3d be-fe/identity-service (be-root)/src/main/resources/application.properties';
-const content = fs.readFileSync(file, 'utf16le');
-fs.writeFileSync(file, content, 'utf8');
-console.log('Fixed application.properties encoding');
--- nailform-project (fe) root/nailform3d/remove_text.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'd:/nailform3d be-fe/nailform-project (fe) root/nailform3d/src/pages/AdminDashboard.jsx';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace(/<span className="admin-stat-meta positive">\s*<i className="fa-solid fa-bolt"><\/i> Real-time DB\s*<\/span>/g, '');
-
-fs.writeFileSync(file, content, 'utf8');
-console.log('Successfully removed Real-time DB text from AdminDashboard.jsx');
